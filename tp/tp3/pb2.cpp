381a45159b5a348c946f3f455c47b81632791020
pablosepulvedasolis <77544623+pablosepulvedasolis@users.noreply.github.com>
2021-09-17 15:33:34 -0400

    Add files via upload

--- tp/tp3/pb2.cpp
+++ tp/tp3/pb2.cpp
@@ -2,35 +2,44 @@
 #define F_CPU 8000000
 #include <util/delay.h> 
 
+#define TOUTSORTIE 0xff;
+#define DIRECTION 0x02; 
+#define COURANT_ALLUMER 0x01;
+#define COURANT_ETEINT 0x00;
+
 void Delay_us(int n) {
     while (n--) {
         _delay_us(1);
     }
 }
 
-int main()
+void moteur_roule(int a, int b, int dec, int repetitions)
 {
-    #define TOUTSORTIE 0xff;
-    #define SWITCH_COURANT 0x01; //Configuration pour changer le courant du moteur
-    #define DIRECTION 0x02; 
-    DDRC = TOUTSORTIE; // Tout les ports de C sont en Sortie
-
-    int a = 0;
-    int b = 16667;
-    int dec = 4167;
-    PORTC = 0x01;
-
-    while (a <= 16667)
+    while (a <= b)
     {
-        for (int i=0; i<4;i++){  
-            for (int j=0; j<120;j++){  
-                PORTC ^= SWITCH_COURANT;
-                Delay_us(a);
-                PORTC ^= SWITCH_COURANT;
-                Delay_us(b-a);
-            }
+        for (int j=0; j<repetitions;j++){  
+            PORTC = COURANT_ALLUMER;
+            Delay_us(a);
+            PORTC = COURANT_ETEINT;
+            Delay_us(b-a);
         }
         a += dec;
     }
+}
+int main()
+{
+    DDRC = TOUTSORTIE; // Tout les ports de C sont en Sortie
+    PORTC = 0x01;
+
+    int a = 0;
+    int b = 16667;
+    int dec = 4166;
+    moteur_roule(a, b, dec, 65);
+
+    a = 0;
+    b = 2500;
+    dec = 500;
+    moteur_roule(a, b, dec, 350);
+    
     return 0; 
 }
