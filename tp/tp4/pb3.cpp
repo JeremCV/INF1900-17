769e187c11101b8523916e0436f9afcb4a908c63
Jeremie Cloutier-Vilhuber <jeremie.cloutier-vilhuber@polymtl.ca>
2021-09-29 10:38:08 -0400

    Final changes

--- tp/tp4/pb3.cpp
+++ tp/tp4/pb3.cpp
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #define F_CPU 8000000
 #include <util/delay.h> 
-#include <avr/interrupt.h>
 
-const uint8_t ENTREE = 0x00;
 const uint8_t SORTIE = 0xff;
 
 
@@ -39,7 +37,7 @@ void stop(){
 }
 
 int main(){
-    initialisation();
+    DDRA = SORTIE;
     ajustementPwm(1*255);
     _delay_ms(2000);
     ajustementPwm(0.75*255);
