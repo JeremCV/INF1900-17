e3f1e2cc9430cc9061f8b6560519516cdb7cf0d8
Jeremie Cloutier-Vilhuber <jeremie.cloutier-vilhuber@polymtl.ca>
2021-10-01 14:02:28 -0400

    Version updated

--- tp/tp5/pb1.cpp
+++ tp/tp5/pb1.cpp
@@ -8,7 +8,8 @@ const uint8_t SORTIE     = 0xff;
 const uint8_t VERT       = 0x01;
 const uint8_t ROUGE      = 0x02;
 const uint8_t ETEINTE    = 0x00;
-uint8_t donnee[45] = {'*','P','*','O','*','L','*','Y','*','T','*','E','*','C','*','H','*','N','*','I','*','Q','*','U','*','E','*',' ','*','M','*','O','*','N','*','T','*','R','*','E','*','A','*','L','*'};
+const int tailleDonnee = 45;
+uint8_t donnee[tailleDonnee] = {'*','P','*','O','*','L','*','Y','*','T','*','E','*','C','*','H','*','N','*','I','*','Q','*','U','*','E','*',' ','*','M','*','O','*','N','*','T','*','R','*','E','*','A','*','L','*'};
 const double  delaiEcriture = 5.0;
 
 
@@ -22,18 +23,18 @@ void lumiere_verte(){
 
 int main(void )
 {   
+    DDRA = SORTIE;
     Memoire24CXXX memoireExt;
     uint16_t addrFin = 0x0000;
 
-    for (int i = 0; i < donnee.size(); i++){
-      memoireExt.ecriture(0x0000 + i*4, donnee[i])
+    for (int i = 0; i < tailleDonnee; i++){
+      memoireExt.ecriture(0x0000 + i*4, donnee[i]);
       _delay_ms(delaiEcriture);
       addrFin += i*4;
     }
     //memoireExt.ecriture(0x0000, donnee, 45);
     memoireExt.ecriture(addrFin, 0x00);
     _delay_ms(5);
-    DDRA = SORTIE;
 
     uint8_t resultatSortie[46];
     memoireExt.lecture(0x0000, resultatSortie, 46);
