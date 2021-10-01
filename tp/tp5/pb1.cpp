7b84e0b8080d786a80068299f0c85946003a3c1e
Jeremie Cloutier-Vilhuber <jeremie.cloutier-vilhuber@polymtl.ca>
2021-10-01 13:51:54 -0400

    Minor changes

--- tp/tp5/pb1.cpp
+++ tp/tp5/pb1.cpp
@@ -2,13 +2,15 @@
 #define F_CPU 8000000
 #include <util/delay.h>
 #include "memoire_24.cpp"
+#include <avr/io.h>
 
 const uint8_t SORTIE     = 0xff;
 const uint8_t VERT       = 0x01;
 const uint8_t ROUGE      = 0x02;
 const uint8_t ETEINTE    = 0x00;
-const uint8_t donnee[] = {"*","P","*","O","*","L","*","Y","*","T","*","E","*","C","*","H","*","N","*","I","*","Q","*","U","*","E","*"," ",
-                          "*","M","*","O","*","N","*","T","*","R","*","E","*","A","*","L","*";}
+uint8_t donnee[45] = {'*','P','*','O','*','L','*','Y','*','T','*','E','*','C','*','H','*','N','*','I','*','Q','*','U','*','E','*',' ','*','M','*','O','*','N','*','T','*','R','*','E','*','A','*','L','*'};
+const double  delaiEcriture = 5.0;
+
 
 void lumiere_rouge(){
     PORTA = ROUGE;
@@ -22,19 +24,28 @@ int main(void )
 {   
     Memoire24CXXX memoireExt;
     uint16_t addrFin = 0x0000;
-    memoireExt.ecriture(0x0000, donnee, 45);
+
+    for (int i = 0; i < donnee.size(); i++){
+      memoireExt.ecriture(0x0000 + i*4, donnee[i])
+      _delay_ms(delaiEcriture);
+      addrFin += i*4;
+    }
+    //memoireExt.ecriture(0x0000, donnee, 45);
     memoireExt.ecriture(addrFin, 0x00);
     _delay_ms(5);
     DDRA = SORTIE;
 
-    uint8_t resultatSortie[];
+    uint8_t resultatSortie[46];
     memoireExt.lecture(0x0000, resultatSortie, 46);
 
     bool pareil = 1;
-    for (int i = 0; i < resultatSortie.length(); i++){
-        if (!(resulatSortie[i] == donnee[i]))
+    for (int i = 0; i < 45; i++){
+        if (!(resultatSortie[i] == donnee[i]))
             pareil = 0;
     }
+
+    if (!(resultatSortie[45] == 0x00))
+        	pareil = 0;
 
     if (pareil)
         lumiere_verte();
